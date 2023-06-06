a7a291fc974ad55d4a702f1d6afd030de750f834
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2023-06-06 11:16:40 +0100

    feat: Add chat room

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -172,7 +172,7 @@ function SignUp() {
                     className="signup-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
                     type="submit"
                     >
-                            Sign In
+                            Sign Up
                     </button>
                     <br/>
                     <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to={LoginRoute}>
